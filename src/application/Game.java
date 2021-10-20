529e0f31ccf88913623c3869f0f2f012bf70157a
Josh Halliwell <josh.halliwell1997@hotmail.com>
2021-10-20 17:49:36 +0100

    App starts with black background and moveable players

---
+++ src/application/Game.java
@@ -0,0 +1,161 @@
+package application;
+
+import java.util.Random;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Game {
+	private static int gameWidth;
+	private static int gameHeight;
+	private static int ballRadius;
+	private static int playerWidth;	
+	private static int playerHeight;	
+	private int playerOneXPos;
+	private int playerOneYPos;
+	private int playerTwoXPos;
+	private int playerTwoYPos;
+	private int playerDx;
+	private int playerDy;	
+	private int ballXPos;
+	private int ballYPos;
+	private int ballDx;
+	private int ballDy;
+	private int scoreOne;
+	private int scoreTwo;
+	private boolean isStarted;
+	private boolean upKeyPressed;
+	private boolean downKeyPressed;
+	private boolean wKeyPressed;
+	private boolean sKeyPressed;
+	
+	
+	Game() {
+		Main main = new Main();
+		gameHeight = main.getHeight();
+		gameWidth = main.getWidth();
+		playerHeight = 100;
+		playerWidth = 15;
+		playerOneXPos = 0;
+		playerOneYPos = gameHeight / 2;
+		playerTwoXPos = gameWidth - playerWidth;
+		playerTwoYPos = gameHeight / 2;
+		playerDx = 5;
+		playerDy = 5;
+		ballXPos = gameWidth / 2;
+		ballYPos = gameHeight / 2;
+		ballDy = 2;
+		ballDx = 2;
+		ballRadius = 10;
+		isStarted = true;
+		scoreOne = 0;
+		scoreTwo = 0;
+	}
+	
+	public void run(GraphicsContext gc) {		
+		
+		// Draw Background
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, gameWidth, gameHeight);
+		
+		movePlayers();
+		
+		gc.setFill(Color.GREEN);
+		gc.fillText("2x = " + playerTwoXPos, gameWidth / 2, 100);
+							
+		if(isStarted) {
+			ballCollision();
+			
+		} else {
+			//reset the ball speed and the direction
+//			ballDx = new Random().nextInt(2) == 0 ? 1: -1;
+//			ballDy = new Random().nextInt(2) == 0 ? 1: -1;
+		}
+		// Update Ball Position
+		ballYPos += ballDy;
+		ballXPos += ballDx;
+				
+		// Draw Ball
+		gc.setFill(Color.WHITE);
+		gc.fillOval(ballXPos, ballYPos, ballRadius, ballRadius);
+		
+		// Draw players
+		gc.fillRect(playerOneXPos, playerOneYPos, playerWidth, playerHeight);
+		gc.fillRect(playerTwoXPos, playerTwoYPos, playerWidth, playerHeight);
+	}
+	
+	private void movePlayers() {
+		if (wKeyPressed) {
+			playerOneYPos -= playerDy;
+		}
+		if (upKeyPressed) {
+			playerTwoYPos -= playerDy;
+		}
+		if (sKeyPressed) {
+			playerOneYPos += playerDy;
+		}
+		if (downKeyPressed) {
+			playerTwoYPos += playerDy;
+		}		
+	}
+	
+	private void ballCollision() {
+		if (ballYPos >= gameHeight || ballYPos <= 0) ballDy *= -1;
+		if (ballXPos == playerTwoXPos - 100/*&& ballYPos > (playerOneYPos - playerHeight) && 
+				ballYPos < (playerOneYPos + playerHeight)*/) ballDx *= -1;
+		if (ballXPos == playerTwoXPos && ballYPos > playerTwoYPos &&
+				ballYPos < (playerTwoYPos - playerHeight)) ballDx *= -1;
+	}
+	
+	public void keyHandler(Scene scene){
+		scene.setOnKeyPressed(e ->
+        {
+            if (e.getCode() == KeyCode.UP)
+            {
+                upKeyPressed = true;
+            }
+            if (e.getCode() == KeyCode.DOWN)
+            {
+                downKeyPressed = true;
+            }
+            if (e.getCode() == KeyCode.W)
+            {
+                wKeyPressed = true;
+            }
+            if (e.getCode() == KeyCode.S)
+            {
+                sKeyPressed = true;
+            }
+        });
+
+        scene.setOnKeyReleased(e ->
+        {
+            if (e.getCode() == KeyCode.UP)
+            {
+                upKeyPressed = false;
+            }
+            if (e.getCode() == KeyCode.DOWN)
+            {
+                downKeyPressed = false;
+            }
+            if (e.getCode() == KeyCode.W)
+            {
+                wKeyPressed = false;
+            }
+            if (e.getCode() == KeyCode.S)
+            {
+                sKeyPressed = false;
+            }
+        });	
+	}
+	
+
+	
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,70 @@
+package application;
+	
+import java.time.Duration;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+
+public class Main extends Application {
+	
+	// Dimension of game 
+	private static final int width = 800;
+	private static final int height = 600;
+	
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("PONG");		
+		Group root = new Group(); // Creating root Group, which can hold a collection of nodes
+		Scene theScene = new Scene(root); // Creating a Scene
+		primaryStage.setScene(theScene);  // A stage can show a scene
+		primaryStage.setResizable(false);
+		
+		Canvas canvas = new Canvas(width, height); // Canvas is what we 'draw' on to
+		root.getChildren().add(canvas);
+		
+		Game pongGame = new Game();
+		
+		GraphicsContext gc = canvas.getGraphicsContext2D(); // use this to draw on to canvas
+		
+		new AnimationTimer()
+	    {
+	        public void handle(long currentNanoTime)
+	        {
+	            pongGame.run(gc);
+	        }
+	    }.start();
+	    
+	    pongGame.keyHandler(theScene);
+	    
+	    primaryStage.show();
+	}
+	
+	/**
+	 * Getters for game width and height 
+	 */
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
